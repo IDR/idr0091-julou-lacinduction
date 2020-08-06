081ebcddf358174f6c7852855aeb552a08ba6996
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-08-06 18:56:50 +0100

    adjust since not all folder required a pattern file

--- scripts/pattern_file_generator.py
+++ scripts/pattern_file_generator.py
@@ -16,8 +16,12 @@ def find_range(value):
     Find the t and c range
     """
     m = re.search('_t(.+?)_c', value)
+    if not m:
+        return -1, -1
     t = m.group(1)
     m = re.search('_c(.+?).tif', value)
+    if not m:
+        return -1, -1
     c = m.group(1)
     return t, c
 
@@ -28,6 +32,11 @@ def generate_pattern_file(directory):
     a pattern file in each directory containing the *.tif images
     """
     for subdir, dirs, files in os.walk(directory):
+        values = subdir.split("/")
+        if len(values) == 1:
+            v = values[0]
+        else:
+            v = values[1]
         pattern = subdir + "/*.tif"
         fnames = sorted(glob.glob(pattern))
         if len(fnames) == 0:
@@ -36,7 +45,11 @@ def generate_pattern_file(directory):
         file_name = name + ".pattern"
 
         t1, c1 = find_range(fnames[0])
+        if t1 == -1 or c1 == -1:
+            continue
         t2, c2 = find_range(fnames[-1])
+        if t2 == -1 or c2 == -1:
+            continue
         value = name + "_t"
         value += "<%s-%s>" % (t1, t2)
         value += "_c<%s-%s>" % (c1, c2)
