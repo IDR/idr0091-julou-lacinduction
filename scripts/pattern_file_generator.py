352853a5834ffad2d28cf2267a82283f7887c6ae
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-08-11 13:00:37 +0100

    update file

--- scripts/pattern_file_generator.py
+++ scripts/pattern_file_generator.py
@@ -4,11 +4,15 @@
 # The top directory containing the folder with images
 # should be specified i.e. Julou_2020_lacInduction_GL_Images
 
+import datetime
 import glob
 import os
 import re
+import shutil
 import sys
 
+base_path = "/uod/idr/filesets/idr0091-julou-lacinduction/"
+generation_path = "/uod/idr/metadata/idr0091-julou-lacinduction/"
 
 def find_range(value):
     """
@@ -30,6 +34,11 @@ def generate_pattern_file(directory):
     Walk through the various directories and generate
     a pattern file in each directory containing the *.tif images
     """
+    date = str(datetime.date.today()).replace("-", "")
+    pattern_location = generation_path + date + "-pattern"
+    if (os.path.exists(pattern_location)):
+        shutil.rmtree(pattern_location)
+    os.mkdir(pattern_location, 0755)
     for subdir, dirs, files in os.walk(directory):
         values = subdir.split("/")
         if len(values) == 1:
@@ -48,12 +57,14 @@ def generate_pattern_file(directory):
         t2, c2 = find_range(fnames[-1])
         if t2 == -1 or c2 == -1:
             continue
-        value = name + "_t"
+        value = base_path
+        value += subdir + "/"
+        value += name + "_t"
         value += "<%s-%s>" % (t1, t2)
         value += "_c<%s-%s>" % (c1, c2)
         value += ".tif"
         print("creating pattern file: %s" % file_name)
-        with open(os.path.join(subdir, file_name), "w") as f:
+        with open(os.path.join(generation_path, file_name), "w") as f:
             f.write(value)
 
 
